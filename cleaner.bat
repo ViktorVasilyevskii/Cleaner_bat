cc49509a2ac43624763e698dfe36a6a0085b1965
v952 <v9522721932@yandex.ru>
2020-05-01 01:41:00 +0300

    added file cleaner.bat - file cleaner windows

---
+++ cleaner.bat
@@ -0,0 +1,8 @@
+del /q /f /s %WINDIR%\Temp\*.*
+del /q /f /s %SYSTEMDRIVE%\Temp\*.*
+del /q /f /s %Temp%\*.*
+del /q /f /s %Tmp%\*.*
+del /q /f /s %WINDIR%\Prefetch\*.*
+del /q /f /s %SYSTEMDRIVE%\*.log
+del /q /f /s %SYSTEMDRIVE%\*.bak
+del /q /f /s %SYSTEMDRIVE%\*.gid
